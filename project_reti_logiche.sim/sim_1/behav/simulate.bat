6a1423ea75edb9cc30baea73801731f3974544e2
Alessandro Griffanti <100707715+AlessandroGriffanti@users.noreply.github.com>
2023-05-10 19:29:23 +0200

    Delete project_reti_logiche.sim/sim_1 directory

--- project_reti_logiche.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\vivado\\Vivado\\2016.1\\bin
-call %xv_path%/xsim project_tb_behav -key {Behavioral:sim_1:Functional:project_tb} -tclbatch project_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
